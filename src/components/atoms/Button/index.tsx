1dce8875490e782e9c97b4b853a78e077f89060f
Donghun kim <kisses217@gmail.com>
2024-02-13 20:53:54 +0900

    first commit

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,18 @@
+import styles from "./index.module.scss";
+
+
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = () => {
+  
+  return (
+    <button
+      type="button"
+      className={styles.button_test}
+    >
+      test
+    </button>
+  );
+};
---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+/* eslint-disable react-refresh/only-export-components */
+import "./styles/_global.scss";
+//* Atom 컴포넌트
+export * from "./components/atoms";
+
+
