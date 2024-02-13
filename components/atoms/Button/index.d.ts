8de5d4155251832884686d84828695cf990c034b
june <ryu9663@naver.com>
2024-02-13 21:43:48 +0900

    Updates

---
+++ components/atoms/Button/index.d.ts
@@ -0,0 +1,4 @@
+/**
+ * Primary UI component for user interaction
+ */
+export declare const Button: () => import("react/jsx-runtime").JSX.Element;
---
+++ components/atoms/Button/index.stories.d.ts
@@ -0,0 +1,13 @@
+import type { StoryObj } from '@storybook/react';
+declare const meta: {
+    title: string;
+    component: () => import("react/jsx-runtime").JSX.Element;
+    parameters: {
+        layout: string;
+    };
+    tags: string[];
+    argTypes: {};
+};
+export default meta;
+type Story = StoryObj<typeof meta>;
+export declare const Default: Story;
