5ad1d4f0d6d3b48af525d97e8d0b4640d5032e1a
Donghun kim <donghun0217@kakao.com>
2024-02-28 19:31:12 +0900

    fix: npm 반영 테스트

--- src/lib/components/Button/index.tsx
+++ src/lib/components/Button/index.tsx
@@ -3,7 +3,7 @@ import styles from './Button.module.scss';
 const Button = () => {
   return (
     <button type="button" className={styles.button_test}>
-      test
+      test버튼
     </button>
   );
 };
