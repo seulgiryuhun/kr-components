ee961d472918f993bce4b3827c58726e67c7fa0a
Donghun kim <donghun0217@kakao.com>
2024-02-28 17:46:27 +0900

    fix: npm 배포 수정

---
+++ src/lib/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Button.module.scss';
+
+const Button = () => {
+  return (
+    <button type="button" className={styles.button_test}>
+      test
+    </button>
+  );
+};
+
+export default Button;
