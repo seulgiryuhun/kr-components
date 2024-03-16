2e8d6a9c4a6df28f7800b371c6fc050640aacc41
june <ryu9663@naver.com>
2024-03-16 23:37:23 +0900

    feat: SC-7 setup file 생성

--- src/lib/components/Button/index.test.tsx
+++ src/lib/components/Button/index.test.tsx
@@ -3,55 +3,54 @@ import { render } from '@testing-library/react';
 import { Button } from './index';
 
 test('정확한 버튼 label을 렌더한다.', async () => {
-  const button = render(<Button variant="filled" theme='gray' />);
+  const button = render(<Button variant="filled" theme="gray" />);
 
   expect(button.getByRole('button')).toBeDefined();
 });
 
 test('filled variant에 따른 버튼을 렌더한다.', async () => {
-  const filledButton = render(<Button variant="filled" theme='gray' />);
+  const filledButton = render(<Button variant="filled" theme="gray" />);
 
   expect(filledButton.getByRole('button').className).toContain('filled');
 });
 
 test('outlined variant에 따른 버튼을 렌더한다.', async () => {
-  const outlinedButton = render(<Button variant="outlined" theme='gray' />);
+  const outlinedButton = render(<Button variant="outlined" theme="gray" />);
 
   expect(outlinedButton.getByRole('button').className).toContain('outlined');
 });
 
 test('text variant에 따른 버튼을 렌더한다.', async () => {
-  const textButton = render(<Button variant="text" theme='gray' />);
+  const textButton = render(<Button variant="text" theme="gray" />);
 
   expect(textButton.getByRole('button').className).toContain('text');
 });
 
 test('small size 버튼을 렌더한다.', async () => {
-  const button = render(<Button theme='gray' size="sm" />);
+  const button = render(<Button theme="gray" size="sm" />);
 
   expect(button.getByRole('button').className).toContain('sm');
 });
 
 test('medium size 버튼을 렌더한다.', async () => {
-  const button = render(<Button theme='gray' size="md" />);
+  const button = render(<Button theme="gray" size="md" />);
 
   expect(button.getByRole('button').className).toContain('md');
 });
 
 test('large size 버튼을 렌더한다.', async () => {
-  const button = render(<Button theme='gray' size="lg" />);
+  const button = render(<Button theme="gray" size="lg" />);
 
   expect(button.getByRole('button').className).toContain('lg');
 });
 
 test('theme에 맞는 버튼을 렌더한다.', async () => {
-  const button = render(<Button theme='gray' />);
+  const button = render(<Button theme="gray" />);
 
   expect(button.getByRole('button').className).toContain('gray');
 });
 
 test('icon을 포함한 버튼을 렌더한다.', async () => {
-  const button = render(<Button variant="outlined"theme='gray' icon='vite' iconSize={8} />);
-
-  expect(button.getByRole('button')).toBeDefined();
+  const button = render(<Button variant="outlined" theme="gray" icon="vite" iconSize={8} />);
+  expect(button.getByRole('button')).toBeInTheDocument();
 });
