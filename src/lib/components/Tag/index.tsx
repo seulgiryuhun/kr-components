6b478d29f42724d9881deed59f6e026ffd53c8ff
june <ryu9663@naver.com>
2024-03-23 02:58:30 +0900

    feat: SC-9 Tag 컴포넌트 기능 구현

--- src/lib/components/Tag/index.tsx
+++ src/lib/components/Tag/index.tsx
@@ -1,10 +1,20 @@
 import classNames from 'classnames';
 import { TagProps } from './type';
 import styles from './index.module.scss';
+import { MouseEventHandler, useState } from 'react';
 
 export const Tag = ({ onClick, className, children, ...props }: TagProps) => {
+  const [isOn, setIsOn] = useState(false);
+  const handleClickTag: MouseEventHandler<HTMLButtonElement> = e => {
+    setIsOn(!isOn);
+    onClick?.(e);
+  };
   return (
-    <button className={classNames(className, styles['tag'])} {...props}>
+    <button
+      className={classNames(className, styles['tag'], styles[`${isOn ? 'on' : 'off'}`])}
+      onClick={handleClickTag}
+      {...props}
+    >
       {children}
     </button>
   );
