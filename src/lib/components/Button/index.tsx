bc5fa5778933329acc6cf1209652c19960954b77
Donghun kim <donghun0217@kakao.com>
2024-03-23 11:42:49 +0900

    feat: SC-4 button props 추가

--- src/lib/components/Button/index.tsx
+++ src/lib/components/Button/index.tsx
@@ -3,20 +3,24 @@ import classNames from 'classnames';
 import styles from './index.module.scss';
 import { Icon } from '../Icon';
 
-export const Button = ({ variant = 'filled', size = 'md', theme = 'gray', icon, iconSize = 0,...props }: ButtonProps) => {
-  const {children} = props
+export const Button = ({
+  variant = 'filled',
+  size = 'md',
+  theme = 'gray',
+  icon,
+  iconSize = 0,
+  ...props
+}: ButtonProps) => {
+  const { children, className } = props;
 
   return (
-    <button 
-      className={classNames(styles.btn, styles[variant], styles[size], styles[theme])}
-      type='button'
+    <button
+      className={classNames(styles.btn, styles[variant], styles[size], styles[theme], className)}
+      type="button"
+      {...props}
     >
-      {
-        icon && <Icon type={icon} size={iconSize}/>
-      }
-      <span>
-        {children}
-      </span>
+      {icon && <Icon type={icon} size={iconSize} />}
+      <span>{children}</span>
     </button>
-    )
+  );
 };
