005172e502f7f55383e0d51b028ee1cbad9d7d1e
june <ryu9663@naver.com>
2024-05-26 14:04:23 +0900

    feat: SC-23 사이드바 열고 닫을때 애니메이션 구현

--- src/lib/components/Sidebar/index.stories.tsx
+++ src/lib/components/Sidebar/index.stories.tsx
@@ -1,45 +1,98 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Sidebar } from '@/lib/components/Sidebar';
 import { Link, MemoryRouter } from 'react-router-dom';
+import { useState } from 'react';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Example/Sidebar',
   component: Sidebar,
-
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {},
+  args: {
+    headerHeight: 0,
+    right: true,
+  },
+  tags: ['autodocs'],
 } satisfies Meta<typeof Sidebar>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof Sidebar>;
 
 export const Default: Story = {
-  args: {
-    links: [],
+  render: args => {
+    return (
+      <MemoryRouter>
+        border는 storybook에서 확인하기 위해 추가함
+        <div
+          style={{
+            border: '1px gray solid',
+            width: '100%',
+            height: '1000px',
+            background: 'gray',
+          }}
+        >
+          <Sidebar
+            isOpen={true}
+            headerHeight={args.headerHeight}
+            onXBtnClick={() => console.log('x')}
+            right={args.right}
+            links={[
+              <Link to="all">all</Link>,
+              <Link to={`/side-project`}>side-project</Link>,
+              <Link to={`/about`}>about</Link>,
+              <Link to={`/component`}>component</Link>,
+              <Link to={`/react`}>react</Link>,
+              <Link to={`/next`}>next</Link>,
+              <Link to={`/browser`}>browser</Link>,
+              <Link to={`/2023`}>2023</Link>,
+              <a href="https://www.spacecloud.kr" target="_blank">
+                외부링크 스클?? ㅋㅋ
+              </a>,
+            ]}
+          />
+        </div>
+      </MemoryRouter>
+    );
   },
-  render: () => (
-    <MemoryRouter>
-      border는 storybook에서 확인하기 위해 추가함
-      <div style={{ border: '1px gray solid', width: 'fit-content', height: '1000px', background: 'gray' }}>
-        <Sidebar
-          onXBtnClick={() => console.log('X')}
-          links={[
-            <Link to="all">all</Link>,
-            <Link to={`/side-project`}>side-project</Link>,
-            <Link to={`/about`}>about</Link>,
-            <Link to={`/component`}>component</Link>,
-            <Link to={`/react`}>react</Link>,
-            <Link to={`/next`}>next</Link>,
-            <Link to={`/browser`}>browser</Link>,
-            <Link to={`/2023`}>2023</Link>,
-            <a href="https://www.spacecloud.kr" target="_blank">
-              외부링크 스클?? ㅋㅋ
-            </a>,
-          ]}
-        />
-      </div>
-    </MemoryRouter>
-  ),
+};
+
+export const Animation: Story = {
+  render: args => {
+    const [isOpen, setIsOpen] = useState(true);
+    return (
+      <MemoryRouter>
+        border는 storybook에서 확인하기 위해 추가함
+        <div
+          style={{
+            border: '1px gray solid',
+            width: '100%',
+            height: '1000px',
+            background: 'gray',
+          }}
+        >
+          <button onClick={() => setIsOpen(true)}>sidebar open</button>
+          <Sidebar
+            isOpen={isOpen}
+            headerHeight={args.headerHeight}
+            onXBtnClick={() => setIsOpen(false)}
+            right={args.right}
+            links={[
+              <Link to="all">all</Link>,
+              <Link to={`/side-project`}>side-project</Link>,
+              <Link to={`/about`}>about</Link>,
+              <Link to={`/component`}>component</Link>,
+              <Link to={`/react`}>react</Link>,
+              <Link to={`/next`}>next</Link>,
+              <Link to={`/browser`}>browser</Link>,
+              <Link to={`/2023`}>2023</Link>,
+              <a href="https://www.spacecloud.kr" target="_blank">
+                외부링크 스클?? ㅋㅋ
+              </a>,
+            ]}
+          />
+        </div>
+      </MemoryRouter>
+    );
+  },
 };
--- src/lib/components/Sidebar/index.tsx
+++ src/lib/components/Sidebar/index.tsx
@@ -3,7 +3,7 @@ import { SidebarProps } from '@/lib/components/Sidebar/types';
 import { useState } from 'react';
 import X from '@/assets/x_icon.svg';
 
-export const Sidebar = ({ links, onXBtnClick }: SidebarProps) => {
+export const Sidebar = ({ links, onXBtnClick, headerHeight = 0, isOpen, right = false }: SidebarProps) => {
   const [selectedItem, setSelectedItem] = useState<number>(0);
   links.forEach(link => {
     if ('href' in link.props || 'to' in link.props) {
@@ -12,25 +12,21 @@ export const Sidebar = ({ links, onXBtnClick }: SidebarProps) => {
     throw Error('a태그가 아닌 다른 태그를 사용하셨습니다.');
   });
   return (
-    <>
-      {
-        <SidebarWrapper>
-          <SidebarHeader>
-            <button onClick={onXBtnClick}>
-              <img src={X} alt="X" />
-            </button>
-          </SidebarHeader>
-          <SidebarNav>
-            <ul>
-              {links.map((link, i) => (
-                <SidebarNavItem key={i} isSelected={selectedItem === i} onClick={() => setSelectedItem(i)}>
-                  {link}
-                </SidebarNavItem>
-              ))}
-            </ul>
-          </SidebarNav>
-        </SidebarWrapper>
-      }
-    </>
+    <SidebarWrapper isOpen={isOpen} headerHeight={headerHeight} right={right}>
+      <SidebarHeader>
+        <button onClick={onXBtnClick}>
+          <img src={X} alt="X" />
+        </button>
+      </SidebarHeader>
+      <SidebarNav>
+        <ul>
+          {links.map((link, i) => (
+            <SidebarNavItem key={i} isSelected={selectedItem === i} onClick={() => setSelectedItem(i)}>
+              {link}
+            </SidebarNavItem>
+          ))}
+        </ul>
+      </SidebarNav>
+    </SidebarWrapper>
   );
 };
