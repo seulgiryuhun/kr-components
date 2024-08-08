97835b4bd247cbbd2ea9c0c66f7809bcfac72d02
Donghun kim <donghun0217@kakao.com>
2024-08-08 18:23:52 +0900

    feat: SC-17 IconButton 컴포넌트 이미지 이슈 대응

--- src/lib/components/Icon/styled.ts
+++ src/lib/components/Icon/styled.ts
@@ -1,13 +1,15 @@
 import styled from '@emotion/styled';
 import { IconProps } from './type';
 
+const imageBaseUrl = process.env.REACT_APP_IMAGE_BASE_URL || '';
+
 export const StyledIcon = styled.i<IconProps>`
   display: inline-flex;
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
 
-  background-image: url(${({ type }) => `./${type}.svg`});
+  background-image: url(${({ type }) => `${imageBaseUrl}/${type}.svg`});
   width: ${({ size }) => size}px;
   height: ${({ size }) => size}px;
 `;
