ebf9f7ce7d11eb283d5b96c4d80e2666bbf71463
Donghun kim <donghun0217@kakao.com>
2024-08-17 19:31:51 +0900

    fix: Icon 이슈 대응

--- src/lib/components/Icon/styled.ts
+++ src/lib/components/Icon/styled.ts
@@ -7,7 +7,7 @@ export const StyledIcon = styled.i<IconProps>`
   background-size: cover;
   background-position: center;
 
-  background-image: url(${({ type }) => `../../${type}.svg`});
+  background-image: url(${({ type }) => `../../../${type}.svg`});
   width: ${({ size }) => size}px;
   height: ${({ size }) => size}px;
 `;
